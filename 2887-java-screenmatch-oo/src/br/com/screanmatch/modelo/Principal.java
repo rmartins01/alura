b2d2955d6088809336ef65b8a3ba13c0d3fe3fe0
Robson Martins <robson.java@gmail.com>
2024-07-09 19:00:26 -0300

    Continuação do curso de Orientação a objetos, capítulo 3

---
+++ 2887-java-screenmatch-oo/src/br/com/screanmatch/modelo/Principal.java
@@ -0,0 +1,21 @@
+package br.com.screanmatch.modelo;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        Filme favorito = new Filme();
+
+        favorito.setNome("The Matrix");
+        favorito.setAnoDeLancamento(1999);
+        favorito.setDuracaoEmMinutos(135);
+        favorito.setIncluidoNoPlano(true);
+
+        favorito.exibeFichaTecnica();
+        favorito.avalia(9);
+        favorito.avalia(8);
+        favorito.avalia(9);
+
+        System.out.println("Média de avaliações do filme: " +favorito.pegaMedia());
+    }
+
+}
