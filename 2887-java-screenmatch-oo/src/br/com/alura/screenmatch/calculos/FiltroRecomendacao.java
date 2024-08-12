b6e9c26a2aa5c6f3baeb508e368e42f442630a5c
Robson Martins <robson.java@gmail.com>
2024-08-12 20:31:45 -0300

    .

--- 2887-java-screenmatch-oo/src/br/com/alura/screenmatch/calculos/FiltroRecomendacao.java
+++ 2887-java-screenmatch-oo/src/br/com/alura/screenmatch/calculos/FiltroRecomendacao.java
@@ -1,7 +1,6 @@
 package br.com.alura.screenmatch.calculos;
 
 public class FiltroRecomendacao {
-    private String recomendacao;
 
     public void filtra(Classificavel classificavel) {
         if (classificavel.getClassificacao() >= 4) {
